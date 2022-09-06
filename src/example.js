5178110481678633e912b4bcb7ebd6efaa38425c
classmatewu <171755422@qq.com>
2022-09-06 21:25:37 +0800

    feat(test): add async test

--- src/example.js
+++ src/example.js
@@ -1,7 +1,39 @@
+const https = require('https')
+
+
 const multiply = (a, b) => {
   return a * b
 }
 
+const fetchData = () => {
+  return new Promise((resolve, reject) => {
+    // const options = {
+    //   hostname: 'opendata.baidu.com',
+    //   port: 443,
+    //   path: '/api.php?query=0.0.0.0&co=&resource_id=6006&oe=utf8',
+    //   method: 'GET'
+    // }
+    const url = "https://opendata.baidu.com/api.php?query=0.0.0.0&co=&resource_id=6006&oe=utf8"
+    
+    const req = https.request(url, res => {
+      console.log(`状态码: ${res.statusCode}`)
+    
+      res.on('data', d => {
+        process.stdout.write(d)
+        resolve(JSON.parse(d.toString()))
+      })
+    })
+
+    req.on('error', error => {
+      console.error(error)
+      resolve(error)
+    })
+    
+    req.end()
+  })
+}
+
 export {
-  multiply
+  multiply,
+  fetchData
 }
---
+++ test/async.test.js
@@ -0,0 +1,12 @@
+import { fetchData } from '../src/example'
+
+test('测试 async await', async () => {
+  const res = await fetchData()
+  expect(res?.data?.[0]?.location).toBe("保留地址")
+})
+
+test('测试 Promise', () => {
+  return fetchData().then(res => {
+    expect(res?.data?.[0]?.location).toBe("保留地址")
+  })
+})
