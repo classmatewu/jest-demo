683cf268d1c40717f7b3a0dea46307262231268a
classmatewu <171755422@qq.com>
2022-09-08 13:38:04 +0800

    upd(snapshot): add command

--- test/snapshot.test.js
+++ test/snapshot.test.js
@@ -1,5 +1,6 @@
 /**
- * 快照测试：通过保存快照，然后前后对比快照的方式，来测试一些不变的数据（配置、UI（html、css等）等）
+ * 快照测试：通过保存快照，然后每次test对比快照的方式，来测试一些不变的数据（配置、UI（html、css等）等）
+ * 若更新了配置，重新 pnpm test 会报错，也可 pnpm test -- -u 去更新快照
  */
 
  import { getConfig } from '../src/example'
